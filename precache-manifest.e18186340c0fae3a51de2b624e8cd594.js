2b1f3e811f76a71e7c6783894c9d4773c44a2ee9
Ashok P A <ashok.p@diagnal.com>
2022-04-13 20:39:32 +0530

    init demo

---
+++ precache-manifest.e18186340c0fae3a51de2b624e8cd594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bf6afa36f9377e02f724f94ddeebb5",
+    "url": "/sendbird/index.html"
+  },
+  {
+    "revision": "36e8df5f687f3743cca8",
+    "url": "/sendbird/static/css/main.e8131b5a.chunk.css"
+  },
+  {
+    "revision": "7ca13c80b74346439cf0",
+    "url": "/sendbird/static/js/2.3bb2cb93.chunk.js"
+  },
+  {
+    "revision": "992ee000ce6422bf8efe3ef20e0be4a9",
+    "url": "/sendbird/static/js/2.3bb2cb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e8df5f687f3743cca8",
+    "url": "/sendbird/static/js/main.c166734b.chunk.js"
+  },
+  {
+    "revision": "b45e3d7979a200cc3e6f",
+    "url": "/sendbird/static/js/runtime-main.dcd8fa41.js"
+  }
+]);
